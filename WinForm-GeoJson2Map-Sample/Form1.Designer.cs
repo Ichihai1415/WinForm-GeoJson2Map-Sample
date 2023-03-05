7e65d273d8af13f517b1ff51129aa975c5369738
Project-S-31415 <projects31415.gif@gmail.com>
2023-03-05 21:54:33 +0900

    プロジェクト ファイルを追加します。

---
+++ WinForm-GeoJson2Map-Sample/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WinForm_GeoJson2Map_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MapImg = new System.Windows.Forms.PictureBox();
+            this.DrawMapB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lat1 = new System.Windows.Forms.NumericUpDown();
+            this.Lon1 = new System.Windows.Forms.NumericUpDown();
+            this.Lat2 = new System.Windows.Forms.NumericUpDown();
+            this.Lon2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.MapImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lat1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lat2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lon2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MapImg
+            // 
+            this.MapImg.Location = new System.Drawing.Point(0, 0);
+            this.MapImg.Name = "MapImg";
+            this.MapImg.Size = new System.Drawing.Size(500, 500);
+            this.MapImg.TabIndex = 0;
+            this.MapImg.TabStop = false;
+            this.MapImg.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MapImg_MouseDown);
+            this.MapImg.MouseEnter += new System.EventHandler(this.MapImg_MouseEnter);
+            this.MapImg.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MapImg_MouseMove);
+            this.MapImg.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MapImg_MouseUp);
+            // 
+            // DrawMapB
+            // 
+            this.DrawMapB.Location = new System.Drawing.Point(400, 0);
+            this.DrawMapB.Name = "DrawMapB";
+            this.DrawMapB.Size = new System.Drawing.Size(75, 23);
+            this.DrawMapB.TabIndex = 2;
+            this.DrawMapB.Text = "描画";
+            this.DrawMapB.UseVisualStyleBackColor = true;
+            this.DrawMapB.Click += new System.EventHandler(this.DrawMapB_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(475, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "北緯　　　　　　°東経　　　　　　°～北緯　　　　　　°東経　　　　　　°";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Lat1
+            // 
+            this.Lat1.DecimalPlaces = 2;
+            this.Lat1.Location = new System.Drawing.Point(32, 2);
+            this.Lat1.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.Lat1.Name = "Lat1";
+            this.Lat1.Size = new System.Drawing.Size(51, 19);
+            this.Lat1.TabIndex = 5;
+            this.Lat1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // Lon1
+            // 
+            this.Lon1.DecimalPlaces = 2;
+            this.Lon1.Location = new System.Drawing.Point(128, 2);
+            this.Lon1.Maximum = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            this.Lon1.Name = "Lon1";
+            this.Lon1.Size = new System.Drawing.Size(51, 19);
+            this.Lon1.TabIndex = 6;
+            this.Lon1.Value = new decimal(new int[] {
+            125,
+            0,
+            0,
+            0});
+            // 
+            // Lat2
+            // 
+            this.Lat2.DecimalPlaces = 2;
+            this.Lat2.Location = new System.Drawing.Point(238, 2);
+            this.Lat2.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.Lat2.Name = "Lat2";
+            this.Lat2.Size = new System.Drawing.Size(51, 19);
+            this.Lat2.TabIndex = 7;
+            this.Lat2.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // Lon2
+            // 
+            this.Lon2.DecimalPlaces = 2;
+            this.Lon2.Location = new System.Drawing.Point(335, 2);
+            this.Lon2.Maximum = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            this.Lon2.Name = "Lon2";
+            this.Lon2.Size = new System.Drawing.Size(51, 19);
+            this.Lon2.TabIndex = 8;
+            this.Lon2.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.DrawMapB);
+            this.Controls.Add(this.Lon2);
+            this.Controls.Add(this.Lat2);
+            this.Controls.Add(this.Lon1);
+            this.Controls.Add(this.Lat1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MapImg);
+            this.Name = "Form1";
+            this.Text = "サンプル";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.MapImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lat1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lat2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lon2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MapImg;
+        private System.Windows.Forms.Button DrawMapB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Lat1;
+        private System.Windows.Forms.NumericUpDown Lon1;
+        private System.Windows.Forms.NumericUpDown Lat2;
+        private System.Windows.Forms.NumericUpDown Lon2;
+    }
+}
+
---
+++ WinForm-GeoJson2Map-Sample/Form1.cs
@@ -0,0 +1,241 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using WinForm_GeoJson2Map_Sample.Properties;
+
+//詳しくはhttps://qiita.com/Ichihai1415/items/8447eb43b3e7c9be4e1a
+namespace WinForm_GeoJson2Map_Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MapImg.MouseWheel += new MouseEventHandler(MapImg_MouseWheel);//←これを追加
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            MapImg.Size = ClientSize;
+            DrawMap();
+        }
+
+        private void DrawMapB_Click(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            json = JObject.Parse(Resources.JsonText);//Parse()の中はFile.ReadAllTextとかResource.とかGeoJSONのstring
+            DrawMap();
+        }
+
+        JObject json;//読み込みとパースを1回だけに
+        Bitmap canvas;//後で別のvoidでさらに描画するとき用(同じvoidでさらに描画するならvoid内でもok)
+        double LatSta = 25;//緯度の始点(=地図の南端)
+        double LatEnd = 50;//緯度の始点(=地図の北端)
+        double LonSta = 125;//経度の始点(=地図の西端)
+        double LonEnd = 150;//経度の始点(=地図の東端)
+        double ZoomW = 0;//横のズーム倍率(画像横ピクセル÷表示する経度の範囲)
+        double ZoomH = 0;//縦のズーム倍率(画像縦ピクセル÷表示する緯度の範囲)
+
+        /// <summary>
+        /// 地図を一気に全部描画します。
+        /// </summary>
+        private void DrawMap()
+        {
+            canvas = new Bitmap(MapImg.Width, MapImg.Height);//pictureBoxの幅と高さ
+            ZoomW = canvas.Width / (LonEnd - LonSta);//経度の差×幅ズーム倍率=画像幅だから幅ズーム倍率=画像幅÷経度の差
+            ZoomH = canvas.Height / (LatEnd - LatSta);//同じように高さズーム倍率=画像高さ÷緯度の差
+            Graphics g = Graphics.FromImage(canvas);
+            g.Clear(Color.FromArgb(120, 120, 255));//背景色
+            GraphicsPath Maps = new GraphicsPath();
+            Maps.StartFigure();
+            foreach (JToken json_1 in json.SelectToken("features"))//各地域のデータにする処理
+            {
+                if ((string)json_1.SelectToken("geometry.type") == "Polygon")//地域が1つのPolygonでできている場合
+                {
+                    List<Point> points = new List<Point>();
+                    foreach (JToken json_2 in json_1.SelectToken($"geometry.coordinates[0]"))
+                        points.Add(new Point((int)(((double)json_2.SelectToken("[0]") - LonSta) * ZoomW), (int)((LatEnd - (double)json_2.SelectToken("[1]")) * ZoomH)));
+                    if (points.Count > 2)
+                        Maps.AddPolygon(points.ToArray());
+                }
+                else//地域が2つ以上のPolygonでできている場合(MultiPolygon)
+                {
+                    foreach (JToken json_2 in json_1.SelectToken($"geometry.coordinates"))
+                    {
+                        List<Point> points = new List<Point>();
+                        foreach (JToken json_3 in json_2.SelectToken("[0]"))
+                            points.Add(new Point((int)(((double)json_3.SelectToken("[0]") - LonSta) * ZoomW), (int)((LatEnd - (double)json_3.SelectToken("[1]")) * ZoomH)));
+                        if (points.Count > 2)
+                            Maps.AddPolygon(points.ToArray());
+                    }
+                }
+            }
+            g.FillPath(new SolidBrush(Color.FromArgb(150, 150, 150)), Maps);//RGB(150, 150, 150)で塗りつぶし
+            g.DrawPath(new Pen(Color.FromArgb(255, 255, 255), 1), Maps);//RGB(255, 255, 255)で太さ1の線を描画(太さ1なら, 1はいらない)
+            //太さ変えるならZoomに応じて変わるようにした方がいい(↓みたいに)軽さ優先なら1で
+            //g.DrawPath(new Pen(Color.FromArgb(255, 255, 255), (float)Math.Log10(Zoom / 5)), Maps);
+            g.Dispose();
+            MapImg.Image = canvas;
+        }
+
+        /// <summary>
+        /// 地図を地域ごとに描画します。
+        /// </summary>
+        private void DrawMap2()
+        {
+            canvas = new Bitmap(MapImg.Width, MapImg.Height);
+            ZoomW = canvas.Width / (LonEnd - LonSta);
+            ZoomH = canvas.Height / (LatEnd - LatSta);
+            Graphics g = Graphics.FromImage(canvas);
+            g.Clear(Color.FromArgb(120, 120, 255));
+            //GraphicsPath Maps = new GraphicsPath();
+            //Maps.StartFigure();
+            foreach (JToken json_1 in json.SelectToken("features"))
+            {
+                GraphicsPath Maps = new GraphicsPath();//GraphicsPathを地域ごとに作り描画
+                Maps.StartFigure();
+                if ((string)json_1.SelectToken("geometry.type") == "Polygon")
+                {
+                    List<Point> points = new List<Point>();
+                    foreach (JToken json_2 in json_1.SelectToken($"geometry.coordinates[0]"))
+                        points.Add(new Point((int)(((double)json_2.SelectToken("[0]") - LonSta) * ZoomW), (int)((LatEnd - (double)json_2.SelectToken("[1]")) * ZoomH)));
+                    if (points.Count > 2)
+                        Maps.AddPolygon(points.ToArray());
+                }
+                else
+                {
+                    foreach (JToken json_2 in json_1.SelectToken($"geometry.coordinates"))
+                    {
+                        List<Point> points = new List<Point>();
+                        foreach (JToken json_3 in json_2.SelectToken("[0]"))
+                            points.Add(new Point((int)(((double)json_3.SelectToken("[0]") - LonSta) * ZoomW), (int)((LatEnd - (double)json_3.SelectToken("[1]")) * ZoomH)));
+                        if (points.Count > 2)
+                            Maps.AddPolygon(points.ToArray());
+                    }
+                }
+                //↓はfeatures[].properties.nameに"福"が含まれていれば黄色にする例
+                //データによってはfeatures.properties.nameじゃないことがあるので要確認
+                if (((string)json_1.SelectToken("properties.name")).Contains("福"))
+                    g.FillPath(new SolidBrush(Color.FromArgb(255, 255, 0)), Maps);
+                else
+                    g.FillPath(new SolidBrush(Color.FromArgb(150, 150, 150)), Maps);
+                g.DrawPath(new Pen(Color.FromArgb(255, 255, 255), 1), Maps);
+            }
+            //g.FillPath(new SolidBrush(Color.FromArgb(150, 150, 150)), Maps);
+            //g.DrawPath(new Pen(Color.FromArgb(255, 255, 255), 1), Maps);
+            g.Dispose();
+            MapImg.Image = canvas;
+        }
+
+        private void MapImg_MouseEnter(object sender, EventArgs e)
+        {
+            MapImg.Focus();
+        }
+
+        Point ClickPoint;//クリックしたところ
+
+        private void MapImg_MouseDown(object sender, MouseEventArgs e)
+        {
+            ClickPoint = e.Location;
+        }
+
+        private void MapImg_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point UpPoint = e.Location;
+            int DiffX = UpPoint.X - ClickPoint.X;//右にやれば+
+            int DiffY = UpPoint.Y - ClickPoint.Y;//下にやれば+
+            LatSta += DiffY / ZoomH;//緯度の差×縦ズーム倍率=画像高さだから下に動かす緯度×縦ズーム倍率=下に動かした分よって下に動かす緯度(下に動かすと最低緯度が高くなるから+=)=下に動かした分÷縦ズーム倍率
+            LatEnd += DiffY / ZoomH;//上端も同じように
+            LonSta -= DiffX / ZoomW;//Latと同じように(右に動かすと最低経度が低くなるから+=)
+            LonEnd -= DiffX / ZoomW;//右端も同じように
+            DrawMap();
+        }
+        /*
+        private void MapImg_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)//前に転がしたとき
+            {
+                if (LatEnd - LatSta > 2 && LonEnd - LonSta > 2)//0以下だと正常に表示できない ↓の変化量に合わせて調整
+                {
+                    LatSta += 1;//範囲を上下左右1度ずつ狭くする
+                    LatEnd -= 1;//変化値が固定だと詳しいところまで拡大したとき微調整が難しくなる
+                    LonSta += 1;//LatのほうではMath.Log10(LatEnd - LatSta + 1) 、LonのほうではMath.Log10(LonEnd - LonSta + 1) がおすすめ
+                    LonEnd -= 1;//↑これだとy=log10(x+1)のグラフのように変化(y:狭くする量(度),x:緯度差・経度差)
+                }
+            }
+            else//後ろに転がしたとき
+            {
+                if (LatEnd - LatSta < 30 && LonEnd - LonSta < 30)//縮小制限を作っとく
+                {
+                    LatSta -= 1;//範囲を上下左右1度ずつ広くする
+                    LatEnd += 1;//変化の勢いは↑と同じ
+                    LonSta -= 1;
+                    LonEnd += 1;
+                }
+            }
+            DrawMap();
+        }*/
+
+        Point MousePoint;//マウス座標
+        private void MapImg_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePoint = e.Location;
+        }
+
+        private void MapImg_MouseWheel(object sender, MouseEventArgs e)
+        {
+            double UpPercent = (double)(MapImg.Height - MousePoint.Y) / MapImg.Height;//カーソルが上にある割合(上端で0、下端で1)
+            double LeftPercent = (double)(MapImg.Width - MousePoint.X) / MapImg.Width;//左にある割合　(double)しないとintに丸められて0になる
+            /*
+            if (e.Delta > 0)
+            {
+                if (LatEnd - LatSta > 2 && LonEnd - LonSta > 2)
+                {//           ↓×0.5が中心だから2に  ↓複雑なのでスルーしてok(間違えてるかも)
+                    LatSta += 2 * (UpPercent);//上に近い=値が小さいほど下端は上がる
+                    LatEnd -= 2 * (1 - UpPercent);//下にある割合
+                    LonSta += 2 * (1 - LeftPercent);//右にある割合 右に近い=値が大きいほど右端は上がる
+                    LonEnd -= 2 * (LeftPercent);//上と同じように2をMath.Log10(LatEnd - LatSta + 1)に変えてもok
+                }
+            }
+            else
+            {
+                if (LatEnd - LatSta < 30 && LonEnd - LonSta < 30)
+                {
+                    LatSta -= 2 * (UpPercent);//↑と同じ
+                    LatEnd += 2 * (1 - UpPercent);
+                    LonSta -= 2 * (1 - LeftPercent);
+                    LonEnd += 2 * (LeftPercent);
+                }
+            }
+            */
+            if (e.Delta > 0)//2の代わりに拡大率に応じて変化用が変わるやつに
+            {
+                if (LatEnd - LatSta > 2 && LonEnd - LonSta > 2)
+                {//           ↓×0.5が中心だから2に  ↓複雑なのでスルーしてok(間違えてるかも)
+                    LatSta += Math.Log10(LatEnd - LatSta + 1) * (UpPercent);//上に近い=値が小さいほど下端は上がる
+                    LatEnd -= Math.Log10(LatEnd - LatSta + 1) * (1 - UpPercent);//下にある割合
+                    LonSta += Math.Log10(LonEnd - LonSta + 1) * (1 - LeftPercent);//右にある割合 右に近い=値が大きいほど右端は上がる
+                    LonEnd -= Math.Log10(LonEnd - LonSta + 1) * (LeftPercent);//上と同じように2をMath.Log10(LatEnd - LatSta + 1)に変えてもok
+                }
+            }
+            else
+            {
+                if (LatEnd - LatSta < 30 && LonEnd - LonSta < 30)
+                {
+                    LatSta -= Math.Log10(LatEnd - LatSta + 1) * (UpPercent);//↑と同じ
+                    LatEnd += Math.Log10(LatEnd - LatSta + 1) * (1 - UpPercent);
+                    LonSta -= Math.Log10(LonEnd - LonSta + 1) * (1 - LeftPercent);
+                    LonEnd += Math.Log10(LonEnd - LonSta + 1) * (LeftPercent);
+                }
+            }
+            DrawMap();
+        }
+    }
+}
